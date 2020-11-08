ce7af1ad272afa45ae85c0655738b5048e490839
Min Dong <anniemindong@gmail.com>
2020-11-08 00:29:54 -0800

    next step: modify home page

--- client/screens/LoginScreen.js
+++ client/screens/LoginScreen.js
@@ -25,7 +25,7 @@ const LoginScreen = navData => {
             hehavior={Platform.OS === "ios" ? "padding" : "height"}
             style={{ flex: 1 }}
         >
-            <ScrollView>
+
                 <Formik
                     initialValues={{
                         email: "",
@@ -96,7 +96,7 @@ const LoginScreen = navData => {
                     )}
 
                 </Formik>
-            </ScrollView>
+
         </KeyboardAvoidingView>
     );
 }
@@ -107,54 +107,54 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#ffffff",
-    },
-    logo: {
+      },
+      logo: {
         alignItems: "center",
         marginBottom: 40,
-    },
-    image: {
+      },
+      image: {
         width: 100,
         height: 100,
-    },
-    input: {
+      },
+      input: {
         width: 300,
         backgroundColor: "#B6BFC4",
         borderRadius: 25,
         padding: 16,
         fontSize: 16,
         marginVertical: 10,
-    },
-    button: {
+      },
+      button: {
         width: 300,
         backgroundColor: "#738289",
         borderRadius: 25,
         marginVertical: 10,
         paddingVertical: 13,
-    },
-    buttonText: {
+      },
+      buttonText: {
         fontSize: 16,
         fontWeight: "500",
         color: "#ffffff",
         textAlign: "center",
-    },
-    registerContainer: {
+      },
+      registerContainer: {
         alignItems: "flex-end",
         justifyContent: "center",
         paddingVertical: 16,
         flexDirection: "row",
-    },
-    registerText: {
+      },
+      registerText: {
         color: "#738289",
         fontSize: 16,
-    },
-    registerButton: {
+      },
+      registerButton: {
         color: "#738289",
         fontSize: 16,
         fontWeight: "bold",
-    },
-    error: {
-        color: 'red'
-    }
+      },
+      error: {
+          color: 'red'
+      }
 });
 
 export default LoginScreen;
